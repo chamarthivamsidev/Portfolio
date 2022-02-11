9e64c73c2d0b2d59810f6329edd77b821ff184b6
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-02-11 09:43:54 +0530

    updated

--- js/index.js
+++ js/index.js
@@ -1,31 +1,36 @@
-//Header fixed
 
-window.onscroll = function () {
-  const docScrollTop = document.documentElement.scrollTop;
-
-  if (window.innerWidth > 991) {
-    if (docScrollTop > 100) {
-      document.querySelector("header").classList.add("fixed");
-    } else {
-      document.querySelector("header").classList.remove("fixed");
-    }
-  }
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+  body.classList.add("disabledScroll");
+};
+cancelBtn.onclick = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  body.classList.remove("disabledScroll");
 };
 
-const hamBurger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
+};
+// const hamBurger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-hamBurger.addEventListener("click", () => {
-  hamBurger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamBurger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+// document.querySelectorAll(".nav-link").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     hamBurger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//   })
+// );
 
 $(document).ready(function () {
   // typing text animation script
